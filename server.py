b32bd9d0b149ecfebc11276f73b6836fd4e711d7
Austin Goebel <argoebel@ualberta.ca>
2020-01-24 15:23:12 -0700

    cleaned up print statements

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import socketserver
 import os
 import sys
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2013 Abram Hindle, Eddie Antonio Santos, Austin Goebel
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -37,27 +37,27 @@ class MyWebServer(socketserver.BaseRequestHandler):
         self.f = ''
         self.baseAddress = "http://127.0.0.1:8080"
         self.data = self.request.recv(1024).strip()
-        print("Got a request of: %s\n" % self.data)
+        # print("Got a request of: %s\n" % self.data)
         # self.request.sendall(bytearray("OK", 'utf-8'))
 
         self.method, self.directory = self.splitRequest(
             self.data.decode())
 
-        print("self.method: ", self.method)
-        print("self.directory: ", self.directory)
+        # print("self.method: ", self.method)
+        # print("self.directory: ", self.directory)
 
         self.code = self.checkMethod(self.method)
 
-        print("self.code: ", self.code)
+        # print("self.code: ", self.code)
 
         self.code, self.contentType, self.fileDir = self.checkDirectory(
             self.directory)
 
-        print("self.code: ", self.code)
-        print("self.contentType: ", self.contentType)
-        print("self.fileDir: ", self.fileDir)
+        # print("self.code: ", self.code)
+        # print("self.contentType: ", self.contentType)
+        # print("self.fileDir: ", self.fileDir)
 
-        print(self.fileDir)
+        # print(self.fileDir)
 
         if self.code == "200 OK\r\n":
             self.f = open(self.fileDir, 'r').read()
@@ -75,16 +75,16 @@ class MyWebServer(socketserver.BaseRequestHandler):
 
         # print("HTTP/1.1 200 OK\r\n" + self.contentType + self.f)
 
-        print(newRequest)
+        # print(newRequest)
         self.request.sendall(newRequest.encode())
 
     def splitRequest(self, requestData):
         stringArr = requestData.split(" ")
-        print(stringArr)
+        # print(stringArr)
         return stringArr[0], stringArr[1]
 
     def checkMethod(self, method):
-        print("CHECK METHOD")
+        # print("CHECK METHOD")
         if method != "GET":
             self.request_is_valid = False
             newRequest = self.generateRequest(
@@ -95,65 +95,27 @@ class MyWebServer(socketserver.BaseRequestHandler):
             return "HTTP/1.1 200 OK\r\n"
 
     def checkDirectory(self, directory):
-        print("CHECK DIR")
+        # print("CHECK DIR")
 
         extension = os.path.splitext(directory)
-        print("extension: ", extension)
+        # print("extension: ", extension)
 
         # calls to localhost:8080/deep
         # REDIRECT 301
         if directory[len(directory)-1] != '/' and (len(directory) > 1) and extension[1] == "":
             directory = self.baseAddress + directory
-            print("301")
+            # print("301")
             return "301 Moved Permanently\r\n", "Content-Type: text/html\r\n" + "Location: " + directory + "/\r\n", ''
 
         # ALL calls BELOW are to localhost:8080 or localhost:8080/deep/
         fileDir = os.getcwd()
         fileDir += "/www" + directory
 
-    #     print("fileDir: ", fileDir)
-
-    #     # check if file exists
-    #     splitFileDir = fileDir.split("/")
-
-    #     print(splitFileDir[len(splitFileDir)-1])
-
-    #     fileSearch = ''
-
-    #     # https://www.tutorialspoint.com/python3/os_walk.htm
-
-    #     if len(directory) > 1:
-    #         for root, dirs, files, in os.walk(os.getcwd() + "/www"):
-    #             for name in files:
-    #                 if name == splitFileDir[len(splitFileDir)-1]:
-    #                     fileSearch = (os.path.join(root, name))
-    #                     print(os.path.join(root, name))
-
-    #             for name in dirs:
-    #                 if name == splitFileDir[len(splitFileDir)-1]:
-    #                     fileSearch = (os.path.join(root, name))
-    #                     print(os.path.join(root, name))
-
-    #         print("Filesearch3: ", fileSearch)
-    #         if fileSearch == '':
-    #             return "404 Page Not Found\r\n", "Content-Type: text/html", '''
-    # <!DOCTYPE html>
-    # <html>
-    # <head>
-    #     <title>404 Page Not Found</title>
-    # </head>
-    # </html>'''
-
-        # extension = os.path.splitext(directory)
-        # print("extension: ", extension)
-
         if extension[1] == "":
             # dealing with either "/", "/deep/" or "/deep" TO "/index.html", "/deep/index.html", or 301 error
             if (fileDir[len(fileDir)-1] == '/'):
                 fileDir += "index.html"
-                print('ONE')
             else:
-                print('TWO')
                 fileDir += "/index.html"
             contentType = "Content-Type: text/html\r\n"
 
@@ -165,19 +127,13 @@ class MyWebServer(socketserver.BaseRequestHandler):
         # ALL calls below are dealing with either "/index.html" or "/base.css" or "deep/deep.css"
         elif extension[1] == ".css":
             contentType = "Content-Type: text/css\r\n"
-            print("extension .css")
+            # print("extension .css")
             # base case: base.css is in www/
             path = os.path.abspath(os.getcwd() + "/www")
 
-            print("fileDir: ", fileDir)
+            # print("fileDir: ", fileDir)
 
-            print(os.listdir(path))
-
-            # for root, dirs, files, in os.walk(os.getcwd() + "/www"):
-            #     for name in files:
-            #         if name == directory[1:]:
-            #             print(os.path.join(root, name))
-            #             fileDir = (os.path.join(root, name))
+            # print(os.listdir(path))
 
             if os.path.isfile(fileDir):
                 return "200 OK\r\n", contentType, fileDir
@@ -198,7 +154,7 @@ class MyWebServer(socketserver.BaseRequestHandler):
     def generateRequest(self, code, contentType, content):
         httpResponse = "HTTP/1.1 " + code + \
             contentType + "\r\n" + content + "\r\n\r\n"
-        print(httpResponse)
+        # print(httpResponse)
         return httpResponse
 
 

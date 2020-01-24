125c99fbee1069325b9ba531417c06f818d92165
Austin Goebel <argoebel@ualberta.ca>
2020-01-23 23:40:31 -0700

    server2 301 errors relocating successfully

--- server2.py
+++ server2.py
@@ -34,6 +34,8 @@ class MyWebServer(socketserver.BaseRequestHandler):
     def handle(self):
         self.request_is_valid = False
         self.code = ""
+        self.f = ''
+        self.baseAddress = "http://localhost:8080"
         self.data = self.request.recv(1024).strip()
         print("Got a request of: %s\n" % self.data)
         # self.request.sendall(bytearray("OK", 'utf-8'))
@@ -41,18 +43,35 @@ class MyWebServer(socketserver.BaseRequestHandler):
         self.method, self.directory = self.splitRequest(
             self.data.decode())
 
+        print("self.method: ", self.method)
+        print("self.directory: ", self.directory)
+
         self.code = self.checkMethod(self.method)
+
+        print("self.code: ", self.code)
 
         self.code, self.contentType, self.fileDir = self.checkDirectory(
             self.directory)
 
+        print("self.code: ", self.code)
+        print("self.contentType: ", self.contentType)
+        print("self.fileDir: ", self.fileDir)
+
         print(self.fileDir)
 
-        self.f = open(self.fileDir, 'r').read()
+        if self.code == "200 OK\r\n":
+            self.f = open(self.fileDir, 'r').read()
+            newRequest = self.generateRequest(
+                self.code, self.contentType, self.f)
+
+        elif self.code == "301 Moved Permanently\r\n":
+            self.fileDir = "Location: " + self.fileDir
+            newRequest = self.generateRequest(
+                self.code, self.contentType, self.fileDir)
 
         # print("HTTP/1.1 200 OK\r\n" + self.contentType + self.f)
 
-        newRequest = self.generateRequest(self.code, self.contentType, self.f)
+        print(newRequest)
         self.request.sendall(newRequest.encode())
 
     def splitRequest(self, requestData):
@@ -66,16 +85,17 @@ class MyWebServer(socketserver.BaseRequestHandler):
             self.request_is_valid = False
             return "405"
         else:
-            return ""
+            return "HTTP/1.1 200 OK\r\n"
 
     def checkDirectory(self, directory):
         print("CHECK DIR")
 
-        # calls to localhost:8080/ or localhost:8080/deep/
+        # calls to localhost:8080/deep/
         # REDIRECT 301
-        if directory[len(directory)-1] == '/':
-
-            return "301 Moved Permanently", "text/html\r\n", ""
+        if directory[len(directory)-1] == '/' and (len(directory) > 1):
+            directory = self.baseAddress + directory
+            print("301")
+            return "301 Moved Permanently\r\n", "text/html\r\n" + "Location: " + directory[:len(directory)-1] + "\r\n", ''
 
         # ALL calls BELOW are to localhost:8080 or localhost:8080/deep
         fileDir = os.getcwd()
@@ -84,35 +104,54 @@ class MyWebServer(socketserver.BaseRequestHandler):
         print("fileDir: ", fileDir)
 
         extension = os.path.splitext(directory)
-
         print("extension: ", extension)
 
         if extension[1] == "":
             # dealing with either "/", or "/deep" TO "/index.html" or "/deep/index.html"
             if (fileDir[len(fileDir)-1] == '/'):
                 fileDir += "index.html"
+                print('ONE')
             else:
+                print('TWO')
                 fileDir += "/index.html"
             contentType = "text/html\r\n"
+            return "200 OK\r\n", contentType, fileDir
 
         # ALL calls below are dealing with either "/index.html" or "/base.css" or "deep/deep.css"
-        elif extension == ".css":
+        elif extension[1] == ".css":
             contentType = "text/css\r\n"
+            print("extension .css")
+            # base case: base.css is in www/
+            path = os.path.abspath(os.getcwd() + "/www")
 
-        elif extension == ".html":
+            print("fileDir: ", fileDir)
+
+            print(os.listdir(path))
+
+            for root, dirs, files, in os.walk(os.getcwd() + "/www"):
+                for name in files:
+                    if name == directory[1:]:
+                        print(os.path.join(root, name))
+                        fileDir = (os.path.join(root, name))
+
+            return "200 OK\r\n", contentType, fileDir
+
+        elif extension[1] == ".html":
             contentType = "text/html\r\n"
+            return "200 OK\r\n", contentType, fileDir
+
         else:
             contentType = "text/html\r\n"
-
-        return "404", contentType, fileDir
+            return "404", contentType, ''
 
     def errorCheck(self, errorCode):
         if errorCode == "":
             return False
 
     def generateRequest(self, code, contentType, content):
-        httpResponse = "HTTP/1.1 " + code + "\r\n" + \
+        httpResponse = "HTTP/1.1 " + code + \
             contentType + "\r\n" + content + "\r\n\r\n"
+        print(httpResponse)
         return httpResponse
 
 
